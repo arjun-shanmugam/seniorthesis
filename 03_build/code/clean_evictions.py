89a8ee1f1c202ffb54691bdaba7c9335b268315c
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2022-08-16 12:32:08 -0400

    Preliminary cleaning of judgement_for_pdu

--- 03_build/code/clean_evictions.py
+++ 03_build/code/clean_evictions.py
@@ -36,28 +36,39 @@ name_replacement_dict = {"Alex Valderrama": "Alex J Valderrama",
                          "Shelly Sankar": "Shelly Ann Sankar",
                          "Stephen; 08 Poitrast": "Stephen Poitrast",
                          "Steven E  Thomas": "Steven E Thomas"}
-evictions_df = evictions_df.replace(name_replacement_dict)
+evictions_df.loc[:, 'court_person'] = evictions_df.loc[:, 'court_person'].replace(name_replacement_dict)
 
-# Clean inconsistencies in judgement values.
-judgement_replacement_dict = {"defendant": "Defendant",
-                              "plaintiff": "Plaintiff",
-                              "unknown": "Unknown"}
+# Clean dispositions.
+# In some rows, there are two dispositions listed. Choose only the last one.
+rows_with_two_dispositions = evictions_df['disposition'].str.count("/") == 4  # Identify them as containing two dates.
+evictions_df.loc[rows_with_two_dispositions, 'disposition'] = evictions_df.loc[rows_with_two_dispositions, 'disposition'].str.split(pat="\d+/\d+/\d+").str[-2]
+
+# TODO: Deal with row 20793, where disposition is missing!
+evictions_df = evictions_df.drop(index=20793)
+
+# If there was a voluntary dismissal according to disposition, specify that in disposition_found
+mask = (evictions_df['disposition'].str.contains("Voluntary Dismissal")) | (evictions_df['disposition'].str.contains("Voluntary Dismissa"))
+evictions_df.loc[mask, 'disposition_found'] = "Voluntary Dismissal"
+
+# Non-voluntary dismissals are considered involuntary.
+mask = (evictions_df['disposition_found'] == "Dismissed")
+evictions_df.loc[mask, 'disposition_found'] = "Involuntary Dismissal"
 
 
-# # Clean dispositions
-# # TODO: Deal with row 20793, where disposition is missing!
-# evictions_df = evictions_df.drop(index=20793)
-# # print(evictions_df.loc[evictions_df['disposition'].str.count("/") == 4]['disposition'])
-#
-# # If there was a voluntary dismissal according to disposition, specify that in disposition_found
-# mask = (evictions_df['disposition'].str.contains("Voluntary Dismissal")) | (evictions_df['disposition'].str.contains("Voluntary Dismissa"))
-# evictions_df.loc[mask, 'disposition_found'] = "Voluntary Dismissal"
-#
-# # If there was an involuntary dismissal according to disposition, specify that in disposition_found.
-# print(evictions_df['disposition'].loc[evictions_df['disposition'] == ""])
-# mask = evictions_df['disposition'].str.contains("Involuntary Dismissal")
-# print(mask)
-# print(mask[(mask != True) & (mask != False)])
-# evictions_df.loc[mask, 'disposition_found'] = "Involuntary Dismissal"
+# Clean judgment_for_pdu.
+evictions_df = evictions_df.rename(columns={'judgment_for_pdu': 'judgement_for_pdu'})
+judgment_for_pdu_replacement_dict = {"unknown": "Unknown",
+                                     "plaintiff": "Plaintiff",
+                                     "defendant": "Defendant"}
+evictions_df.loc[:, "judgement_for_pdu"] = evictions_df.loc[:, "judgement_for_pdu"].replace(judgment_for_pdu_replacement_dict)
 
-evictions_df.to_stata(OUTPUT_DATA)
+# Generate new column, defendant_victory.
+evictions_df.insert(len(evictions_df.columns), column='defendant_victory', value=0)
+mask = (
+    (evictions_df['judgement_for_pdu'] == "Defendant") |
+    (evictions_df['disposition_found'] == "Involuntary Dismissal")
+)
+evictions_df.loc[mask, 'defendant_victory'] = 1
+
+
+evictions_df.to_stata(OUTPUT_DATA, write_index=False)
