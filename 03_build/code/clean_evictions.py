53aae94591232e9dc35055b798d0cdc040e249ae
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2022-08-16 10:50:14 -0400

    Cleaned judge names

--- 03_build/code/clean_evictions.py
+++ 03_build/code/clean_evictions.py
@@ -4,7 +4,6 @@ clean_evictions.py
 Cleans eviction dataset from MassLandlords.
 """
 import pandas as pd
-import numpy as np
 INPUT_DATA = "/Users/arjunshanmugam/Documents/GitHub/seniorthesis/03_build/input/shanmugam_aug_v2.csv"
 OUTPUT_DATA = "/Users/arjunshanmugam/Documents/GitHub/seniorthesis/03_build/intermediate/evictions.dta"
 evictions_df = pd.read_csv(INPUT_DATA)
@@ -44,20 +43,21 @@ judgement_replacement_dict = {"defendant": "Defendant",
                               "plaintiff": "Plaintiff",
                               "unknown": "Unknown"}
 
-# Clean dispositions
-# TODO: Deal with row 20793, where disposition is missing!
-evictions_df = evictions_df.drop(index=20793)
-# print(evictions_df.loc[evictions_df['disposition'].str.count("/") == 4]['disposition'])
 
-# If there was a voluntary dismissal according to disposition, specify that in disposition_found
-mask = (evictions_df['disposition'].str.contains("Voluntary Dismissal")) | (evictions_df['disposition'].str.contains("Voluntary Dismissa"))
-evictions_df.loc[mask, 'disposition_found'] = "Voluntary Dismissal"
-
-# If there was an involuntary dismissal according to disposition, specify that in disposition_found.
-print(evictions_df['disposition'].loc[evictions_df['disposition'] == ""])
-mask = evictions_df['disposition'].str.contains("Involuntary Dismissal")
-print(mask)
-print(mask[(mask != True) & (mask != False)])
-evictions_df.loc[mask, 'disposition_found'] = "Involuntary Dismissal"
+# # Clean dispositions
+# # TODO: Deal with row 20793, where disposition is missing!
+# evictions_df = evictions_df.drop(index=20793)
+# # print(evictions_df.loc[evictions_df['disposition'].str.count("/") == 4]['disposition'])
+#
+# # If there was a voluntary dismissal according to disposition, specify that in disposition_found
+# mask = (evictions_df['disposition'].str.contains("Voluntary Dismissal")) | (evictions_df['disposition'].str.contains("Voluntary Dismissa"))
+# evictions_df.loc[mask, 'disposition_found'] = "Voluntary Dismissal"
+#
+# # If there was an involuntary dismissal according to disposition, specify that in disposition_found.
+# print(evictions_df['disposition'].loc[evictions_df['disposition'] == ""])
+# mask = evictions_df['disposition'].str.contains("Involuntary Dismissal")
+# print(mask)
+# print(mask[(mask != True) & (mask != False)])
+# evictions_df.loc[mask, 'disposition_found'] = "Involuntary Dismissal"
 
 evictions_df.to_stata(OUTPUT_DATA)
