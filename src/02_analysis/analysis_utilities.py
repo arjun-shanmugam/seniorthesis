4a4f2ff92b3a1f57111846067c1952d5852db757
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2023-01-16 21:13:43 -0500

    Updated Zestimates in summary statistics

--- src/02_analysis/analysis_utilities.py
+++ src/02_analysis/analysis_utilities.py
@@ -101,87 +101,99 @@ def produce_summary_statistics(df: pd.DataFrame, treatment_date_variable: str):
     :param treatment_date_variable:
     :return:
     """
-    # Panel A: Case Initiaton
-    panel_A_columns = ['for_cause', 'no_cause', 'non_payment']
+    # Panel A: Pre-treatment Zestimates
+    df.loc[:, 'change_in_zestimates_2018_to_2019'] = df['2019-01'] - df['2018-01']
+    panel_A_columns = ['2018-01', 'change_in_zestimates_2018_to_2019']
+    panel_A = df[panel_A_columns].describe().T
+    panel_A = pd.concat([panel_A], keys=["Panel A: Pre-treatment Zestimates"])
+
+    # Panel B: Census Tract Characteristics
+    panel_B_columns = ['med_hhinc2016', 'popdensity2010', 'share_white2010']
+    panel_B = df[sorted(panel_B_columns)].describe().T
+    panel_B = pd.concat([panel_B], keys=["Panel B: Census Tract Characteristics"])
+
+    # Panel C: Case Initiaton
+    panel_C_columns = ['for_cause', 'no_cause', 'non_payment']
     origin_columns = ['initiating_action', 'initiating_action', 'initiating_action']
     target_values = [("SP Summons and Complaint - Cause", "SP Transfer - Cause"),
                      ("SP Summons and Complaint - No Cause", "SP Transfer- No Cause"),
                      ("SP Summons and Complaint - Non-payment of Rent", "SP Transfer - Non-payment of Rent")]
 
-    for dummy_column, origin_column, target_value in zip(panel_A_columns, origin_columns, target_values):
+    for dummy_column, origin_column, target_value in zip(panel_C_columns, origin_columns, target_values):
         df.loc[:, dummy_column] = np.where((df[origin_column].str.contains(target_value[0])) |
                                            (df[origin_column].str.contains(target_value[1])),
                                            1,
                                            0)
+    panel_C = df[sorted(panel_C_columns)].describe().T
+    panel_C = pd.concat([panel_C], keys=["Panel C: Case Initiation"])
 
-    panel_A = df[sorted(panel_A_columns)].describe().T
-    panel_A = pd.concat([panel_A], keys=["Panel A: Case Initiation"])
+    # Panel D: Defendant and Plaintiff Characteristics
+    panel_D_columns = ['hasAttyD', 'isEntityD', 'hasAttyP', 'isEntityP']
+    panel_D = df[sorted(panel_D_columns)].describe().T
+    panel_D = pd.concat([panel_D], keys=["Panel D: Defendant and Plaintiff Characteristics"])
 
-    # Panel B: Case Resolution
-    panel_B_columns = ['mediated', 'dismissed', 'defaulted', 'heard']
+    # Panel E: Case Resolution
+    panel_E_columns = ['mediated', 'dismissed', 'defaulted', 'heard']
     origin_columns = ['disposition_found', 'disposition_found', 'disposition_found',
                       'disposition_found']
     target_values = ["Mediated", "Dismissed", "Defaulted", "Heard"]
 
-    for dummy_column, origin_column, target_value in zip(panel_B_columns, origin_columns, target_values):
+    for dummy_column, origin_column, target_value in zip(panel_E_columns, origin_columns, target_values):
         df.loc[:, dummy_column] = np.where(df[origin_column] == target_value, 1, 0)
-
-    # Add case duration and money judgment to Panel B.
-    panel_B_columns.append('case_duration')
-    panel_B_columns.append('judgment')
-    panel_B = df[sorted(panel_B_columns)].describe().T
-    panel_B = pd.concat([panel_B], keys=["Panel B: Case Resolution"])
-
-    # Panel C: Defendant and Plaintiff Characteristics
-    panel_C_columns = ['hasAttyD', 'isEntityD', 'hasAttyP', 'isEntityP']
-    panel_C = df[sorted(panel_C_columns)].describe().T
-    panel_C = pd.concat([panel_C], keys=["Panel C: Defendant and Plaintiff Characteristics"])
-
-    """
-    # Panel D: Tax Assessment Records From F.Y. Following Eviction Filing
-    panel_D_columns = ['TOTAL_VAL', 'BLDG_VAL', 'LAND_VAL', 'OTHER_VAL']
-    panel_D = df[sorted(panel_D_columns)].describe().T
-    panel_D = pd.concat([panel_D], keys=["Panel D: Assessor Records"])
-    """
-
-    # Panel E: Census Tract Characteristics
-    panel_E_columns = ['med_hhinc2016', 'popdensity2010', 'share_white2010']
+    # Add case duration and money judgment to Panel E.
+    panel_E_columns.append('case_duration')
+    panel_E_columns.append('judgment')
     panel_E = df[sorted(panel_E_columns)].describe().T
-    panel_E = pd.concat([panel_E], keys=["Panel E: Census Tract Characteristics"])
+    panel_E = pd.concat([panel_E], keys=["Panel E: Case Resolution"])
 
-    # Panel F: Zestimates Before Filing
-    df.loc[:, 'change_in_zestimates_2018_to_2019'] = df['2019-01'] - df['2018-01']
-    panel_F_columns = ['2018-01', 'change_in_zestimates_2018_to_2019']
+    # Panel F: Post-treatment Zestimates
+    # Get month of the latest docket date for each row and use to grab Zestimates at different times prior to treatment.
+    df.loc[:, treatment_date_variable] = pd.to_datetime(df[treatment_date_variable])
+    df.loc[:, 'nan'] = np.nan
+    panel_F_columns = []
+    start = 0
+    stop = 4
+    for i in range(start, stop):
+        # This column contains the year-month which is i years relative to treatment for each property.
+        offset_docket_month = (df[treatment_date_variable] + pd.tseries.offsets.DateOffset(years=i)).dt.strftime(
+            '%Y-%m').copy()
+
+        # Some year-months will be outside the range of our data.
+        # For instance, we do not have Zestimates 2 years post-treatment for evictions which occurred in 2022.
+        # For these observations, the offset docket month needs to map to the column of nans we created earlier.
+        offset_docket_month.loc[~offset_docket_month.isin(df.columns)] = 'nan'
+
+        # Set column accordingly.
+        idx, cols = pd.factorize(offset_docket_month)
+        if i < 0:
+            new_col_name = f'zestimate_minus{-1*i}_years_relative_to_treatment'
+        else:
+            new_col_name = f'zestimate_{i}_years_relative_to_treatment'
+        panel_F_columns.append(new_col_name)
+        df.loc[:, new_col_name] = df.reindex(cols, axis=1).to_numpy()[np.arange(len(df)), idx]
     panel_F = df[panel_F_columns].describe().T
-    panel_F = pd.concat([panel_F], keys=["Panel F: Pretreatment Zestimates"])
+    panel_F = pd.concat([panel_F], keys=["Panel F: Post-treatment Zestimates"])
+
+
+
 
     # Concatenate Panels A-E
-    summary_statistics = pd.concat([panel_A, panel_B, panel_C, panel_E, panel_F], axis=0)[
-        ['mean', '50%', 'std', 'count']]
+    summary_statistics = pd.concat([panel_A, panel_B, panel_C, panel_D, panel_E, panel_F],
+                                   axis=0)[['mean', '50%', 'std', 'count']]
 
     variable_display_names_dict = {'for_cause': "For cause", 'no_cause': "No cause",
                                    'non_payment': "Non-payment of rent",  # Panel A
                                    'case_duration': "Case duration", 'defaulted': "Case defaulted",
-                                   'heard': "Case heard",  # Panel B
+                                   'heard': "Case heard",
                                    'judgment': "Money judgment", 'mediated': "Case mediated",
-                                   'dismissed': 'Case dismised',  # Panel B
+                                   'dismissed': 'Case dismised',
                                    'hasAttyD': "Defendant has an attorney", 'hasAttyP': "Plaintiff has an attorney",
-                                   # Panel C
                                    'isEntityD': "Defendant is an entity", 'isEntityP': "Plaintiff is an entity",
-                                   # Panel C
-                                   'TOTAL_VAL': "Total property value", 'BLDG_VAL': "Building value",
-                                   'LAND_VAL': "Land value",  # Panel D
-                                   'OTHER_VAL': "Other value",  # Panel D
                                    'med_hhinc2016': 'Median household income (2016)',
                                    'popdensity2010': 'Population density (2010)',  # Panel E
                                    'share_white2010': 'Portion white (2010)',  # Panel E
                                    '2018-01': 'Jan. 2018', 'change_in_zestimates_2018_to_2019': 'Change from Jan. 2018 to Jan. 2019',
-                                   'zestimate_-5_years_relative_to_treatment': "Five years before filing date",
-                                   'zestimate_-4_years_relative_to_treatment': "Four years before filing date",
-                                   'zestimate_-3_years_relative_to_treatment': "Three years before filing date",
-                                   'zestimate_-2_years_relative_to_treatment': "Two years before filing date",
-                                   'zestimate_-1_years_relative_to_treatment': "One year before filing date",
-                                   'zestimate_0_years_relative_to_treatment': "Filing date",
+                                   'zestimate_0_years_relative_to_treatment': "At filing date",
                                    'zestimate_1_years_relative_to_treatment': "One year after filing date",
                                    'zestimate_2_years_relative_to_treatment': "Two years after filing date",
                                    'zestimate_3_years_relative_to_treatment': "Three years after filing date"}
