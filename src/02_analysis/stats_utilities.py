7521f677b266df2f5a95d6d4cada2af2d83d7922
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2023-04-12 02:29:02 -0400

    generated figures

--- src/02_analysis/stats_utilities.py
+++ src/02_analysis/stats_utilities.py
@@ -254,11 +254,11 @@ def produce_summary_statistics(df: pd.DataFrame, treatment_date_variable: str):
     panel_D = pd.concat([panel_D], keys=["Panel D: Defendant and Plaintiff Characteristics"])
 
     # Panel E: Case Resolution
-    panel_E_columns = ['mediated', 'dismissed', 'defaulted', 'heard', 'levied', 'issued', 'requested',
+    panel_E_columns = ['dismissed', 'defaulted', 'heard', 'levied', 'issued', 'requested',
                        'unknown_execution']
-    origin_columns = ['disposition_found', 'disposition_found', 'disposition_found',
+    origin_columns = ['disposition_found', 'disposition_found',
                       'disposition_found', 'execution', 'execution', 'execution', 'execution']
-    target_values = ["Mediated", "Dismissed", "Defaulted", "Heard", "Levied", "Issued", "Requested", None]
+    target_values = ["Dismissed", "Defaulted", "Heard", "Levied", "Issued", "Requested", None]
 
     for dummy_column, origin_column, target_value in zip(panel_E_columns, origin_columns, target_values):
         df.loc[:, dummy_column] = np.where(df[origin_column] == target_value, 1, 0)
