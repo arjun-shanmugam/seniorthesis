95f4d995b7b6e707aa582ce5beb16fc6eaf39bdd
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2023-04-12 22:13:53 -0400

    placebo figure

--- src/02_analysis/constants.py
+++ src/02_analysis/constants.py
@@ -1,9 +1,5 @@
 class Variables:
     outcomes = ['group_0_crimes_500m',
                 'group_1_crimes_500m',
-                'group_2_crimes_500m', 
-                'group_3_crimes_500m',
-                'group_4_crimes_500m',
-                'group_5_crimes_500m',
-                'group_6_crimes_500m']
+                'group_2_crimes_500m']
 
